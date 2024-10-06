a831e43acae57862bd83e2f0972c737263dff404
bbarisic <bojan.barisic@nais.hr>
2024-10-06 08:04:55 +0200

    Added Ticket columns configuration

---
+++ King.Carrier.Infrastructure/Persistence/Configurations/Tickets/TicketConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using King.Carrier.TicketsDomain.Entities.Tickets;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace King.Carrier.TicketsInfrastructure.Persistence.Configurations.Tickets;
+
+public class TicketConfiguration : IEntityTypeConfiguration<Ticket>
+{
+    public void Configure(EntityTypeBuilder<Ticket> entity)
+    {
+        entity.HasKey(e => e.Id)
+             .HasName("Ticket_PK");
+
+        entity.Property(e => e.SerialNumber)
+             .HasMaxLength(150)
+             .IsRequired();
+
+        entity.Property(e => e.CreatedTimestamp)
+            .IsRequired();
+
+        entity.Property(e => e.Price)
+            .HasColumnType("decimal(10,2)")
+            .IsRequired();
+
+        entity.Property(e => e.StartLocation)
+            .HasMaxLength(150)
+            .IsRequired();
+
+        entity.Property(e => e.EndLocation)
+            .HasMaxLength(150)
+            .IsRequired();
+    }
+}
