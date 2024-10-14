986a3efe425cd6cde95affc986a06f2a0d40f1a3
bbarisic <bojan.barisic17@gmail.com>
2024-10-14 08:23:34 +0200

    Implemented hybrid cache

---
+++ King.Carrier.Infrastructure/Integrations/Caching/HybridCache/HybridCache.cs
@@ -0,0 +1,49 @@
+﻿using King.Carrier.TicketsApplication.Integrations.Caching.HybridCache;
+using Microsoft.Extensions.Caching.Memory;
+using StackExchange.Redis;
+
+namespace King.Carrier.TicketsInfrastructure.Integrations.Caching.HybridCache;
+
+public class HybridCache : IHybridCache
+{
+    private readonly IMemoryCache _memoryCache;
+    private readonly IDatabase _redisDb;
+
+    public HybridCache(IMemoryCache memoryCache, IConnectionMultiplexer redis)
+    {
+        _memoryCache = memoryCache;
+        _redisDb = redis.GetDatabase();
+    }
+
+    public async Task<T?> GetAsync<T>(string key)
+    {
+        if (_memoryCache.TryGetValue(key, out T value))
+        {
+            return value;
+        }
+
+        var redisValue = await _redisDb.StringGetAsync(key);
+        if (redisValue.IsNull)
+        {
+            return default;
+        }
+
+        value = System.Text.Json.JsonSerializer.Deserialize<T>(redisValue!)!;
+        _memoryCache.Set(key, value);
+
+        return value;
+    }
+
+    public async Task RemoveAsync(string key)
+    {
+        _memoryCache.Remove(key);
+        await _redisDb.KeyDeleteAsync(key);
+    }
+
+    public async Task SetAsync<T>(string key, T value, TimeSpan expiration)
+    {
+        _memoryCache.Set(key, value, expiration);
+
+        await _redisDb.StringSetAsync(key, System.Text.Json.JsonSerializer.Serialize(value), expiration);
+    }
+}
--- King.Carrier.Infrastructure/Integrations/RabbitMQ/Tickets/TicketPublisher.cs
+++ King.Carrier.Infrastructure/Integrations/RabbitMQ/Tickets/TicketPublisher.cs
@@ -1,4 +1,5 @@
-﻿using King.Carrier.TicketsApplication.Integrations.RabbitMQ.Tickets;
+﻿using King.Carrier.TicketsApplication.Integrations.Caching.HybridCache;
+using King.Carrier.TicketsApplication.Integrations.RabbitMQ.Tickets;
 using King.Carrier.TicketsApplication.Integrations.TicketsApi.RabbitMq;
 using RabbitMQ.Client;
 using System.Text;
@@ -9,16 +10,20 @@ namespace King.Carrier.TicketsInfrastructure.Integrations.RabbitMQ.Tickets;
 public class TicketPublisher : ITicketsPublisher
 {
     private readonly RabbitMqSetupService _rabbitMqSetupService;
+    private readonly IHybridCache _hybridCache;
 
-    public TicketPublisher(RabbitMqSetupService rabbitMqSetupService)
+    public TicketPublisher(RabbitMqSetupService rabbitMqSetupService, IHybridCache hybridCache)
     {
         _rabbitMqSetupService = rabbitMqSetupService;
+        _hybridCache = hybridCache;
     }
 
     public async Task<bool> SendMessage(TicketMessage message)
     {
         var messageSerialized = JsonSerializer.Serialize(message);
         var body = Encoding.UTF8.GetBytes(messageSerialized);
+
+        await _hybridCache.SetAsync(message.TicketNumber, "Ticket", TimeSpan.FromMinutes(15));
 
         _rabbitMqSetupService.GetChannel().BasicPublish(exchange: "carrier_exchange",
                               routingKey: "ticket",
